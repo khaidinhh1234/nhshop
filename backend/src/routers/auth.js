1bcf7ed756de36d78e78812a8453f27e77eddb25
khaidinhh1234 <khaindph36466@fpt.edu.vn>
2024-04-12 00:36:07 +0700

    update fe

--- backend/src/routers/auth.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signin, signup } from "../controllers/signup";
-
-const router = express.Router();
-
-router.post("/auth/signup", signup);
-router.post("/auth/signin", signin);
-export default router;
